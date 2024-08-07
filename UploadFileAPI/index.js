64d09c22edb89a32fa30c2a493f15a3561077da6
Raj Sorathiya <rajsorathiya.acusion@gmail.com>
2024-08-07 15:34:40 +0530

    update

--- UploadFileAPI/index.js
+++ UploadFileAPI/index.js
@@ -11,7 +11,7 @@ const uri = 'mongodb+srv://rajsorathiyaacusion:rajacusion@cluster0.z8vtgf7.mongo
 
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,".uploads/")
+        cb(null,'.public/uploads/')
     },
     filename:function(req,file,cb){
         cb(null,file.originalname)
